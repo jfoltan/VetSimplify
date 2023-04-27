2ece11dd31d90cb3458c169fee545fa5ae69b63b
jfoltan <jakub.foltan41@gmail.com>
2023-04-28 00:45:04 +0200

    refractor views.py with mixins

--- records/views.py
+++ records/views.py
@@ -1,4 +1,3 @@
-from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
@@ -9,22 +8,32 @@ from django.views.generic import (
 )
 from .forms import OwnerForm, AnimalForm
 from .models import Owner, Animal
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class OwnerMixin:
+    def get_owner(self):
+        owner_id = self.kwargs["owner_id"]
+        return get_object_or_404(Owner, pk=owner_id)
+
+
+class AnimalMixin(OwnerMixin):
+    def get_animal(self):
+        owner = self.get_owner()
+        animal_id = self.kwargs["animal_id"]
+        return get_object_or_404(Animal, owner=owner, pk=animal_id)
 
 
 class OwnerListView(ListView):
     model = Owner
 
 
-class OwnerDetailView(DetailView):
+class OwnerDetailView(OwnerMixin, DetailView):
     model = Owner
 
     def get_object(self):
-        owner_id = self.kwargs["owner_id"]
-        return get_object_or_404(Owner, pk=owner_id)
+        return self.get_owner()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -38,83 +47,80 @@ class OwnerCreateView(CreateView):
     template_name = "records/owner_form.html"
 
     def get_success_url(self):
-        return reverse_lazy("records:owner_detail", kwargs={"owner_id": self.object.pk})
+        return reverse("records:owner_detail", kwargs={"owner_id": self.object.pk})
 
 
-class OwnerUpdateView(UpdateView):
+class OwnerUpdateView(OwnerMixin, UpdateView):
     model = Owner
     form_class = OwnerForm
     template_name = "records/owner_update_form.html"
 
     def get_object(self):
-        owner_id = self.kwargs["owner_id"]
-        return get_object_or_404(Owner, pk=owner_id)
+        return self.get_owner()
 
     def get_success_url(self):
-        return reverse_lazy("records:owner_detail", kwargs={"owner_id": self.object.pk})
+        return reverse("records:owner_detail", kwargs={"owner_id": self.object.pk})
 
 
-class OwnerDeleteView(DeleteView):
+class OwnerDeleteView(OwnerMixin, DeleteView):
     model = Owner
-    success_url = reverse_lazy("records:owner_list")
+
+    def get_object(self):
+        return self.get_owner()
+
+    def get_success_url(self):
+        return reverse("records:owner_list")
 
 
-class AnimalCreateView(CreateView):
+class AnimalCreateView(OwnerMixin, CreateView):
     model = Animal
     form_class = AnimalForm
     template_name = "records/animal_form.html"
 
     def form_valid(self, form):
-        owner_id = self.kwargs["owner_id"]
-        owner = get_object_or_404(Owner, pk=owner_id)
+        owner = self.get_owner()
         form.instance.owner = owner
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["owner"] = get_object_or_404(Owner, pk=self.kwargs["owner_id"])
+        context["owner"] = self.get_owner()
         return context
 
     def get_success_url(self):
-        return reverse_lazy(
+        return reverse(
             "records:owner_detail", kwargs={"owner_id": self.kwargs["owner_id"]}
         )
 
 
-class AnimalDetailView(DetailView):
+class AnimalDetailView(AnimalMixin, DetailView):
     model = Animal
 
     def get_object(self):
-        owner_id = self.kwargs["owner_id"]
-        animal_id = self.kwargs["animal_id"]
-        return get_object_or_404(Animal, owner_id=owner_id, pk=animal_id)
+        return self.get_animal()
 
 
-class AnimalUpdateView(UpdateView):
+class AnimalUpdateView(AnimalMixin, UpdateView):
     model = Animal
     form_class = AnimalForm
     template_name = "records/animal_update_form.html"
 
     def get_object(self):
-        owner_id = self.kwargs["owner_id"]
-        animal_id = self.kwargs["animal_id"]
-        return get_object_or_404(Animal, owner_id=owner_id, pk=animal_id)
+        return self.get_animal()
 
     def get_success_url(self):
-        return reverse_lazy(
+        return reverse(
             "records:owner_detail", kwargs={"owner_id": self.kwargs["owner_id"]}
         )
 
 
-class AnimalDeleteView(DeleteView):
+class AnimalDeleteView(AnimalMixin, DeleteView):
     model = Animal
 
     def get_object(self):
-        owner_id = self.kwargs["owner_id"]
-        animal_id = self.kwargs["animal_id"]
-        return get_object_or_404(Animal, owner_id=owner_id, pk=animal_id)
+        return self.get_animal()
 
     def get_success_url(self):
-        return reverse_lazy(
+        return reverse(
             "records:owner_detail", kwargs={"owner_id": self.kwargs["owner_id"]}
         )
