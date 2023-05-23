4ea113eb8a3a42ecef29aeae0c118e6d66ff7f63
jfoltan <jakub.foltan41@gmail.com>
2023-05-23 18:02:49 +0200

    delete unicorns urls

--- vetsimplify/urls.py
+++ vetsimplify/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("unicorn/", include("django_unicorn.urls")),
     path("", include("django.contrib.auth.urls")),
     path("", include("frontend_pages.urls")),
     path("", include("accounts.urls")),
